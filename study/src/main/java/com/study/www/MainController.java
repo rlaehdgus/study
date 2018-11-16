e78dfb3da4e9f7344ca2c7e67ba1ec517dda6fe8
rlaehdgus529 <rlaehdgus529@210.91.69.49>
2018-11-16 16:52:04 +0900

    DH: 메인 페이지 URL설정 및 페이지 출력

---
+++ study/src/main/java/com/study/www/MainController.java
@@ -0,0 +1,29 @@
+package com.study.www;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class MainController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(MainController.class);
+	
+	@RequestMapping(value = {"/", "/main.do"}, method = RequestMethod.GET)
+	public String home() {
+		logger.info("main start!");
+		
+		return "Main/main";
+	}
+	
+}
---
+++ study/src/main/webapp/WEB-INF/views/Main/main.jsp
@@ -0,0 +1,21 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Main</title>
+	</head>
+	<body>
+		<header>
+			헤더
+		</header>
+		
+		<section>
+			몸통
+		</section>
+		
+		<footer>
+			하단
+		</footer>
+	</body>
+</html>
